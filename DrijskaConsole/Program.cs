c3337022c5c4be8a236d6f8e94964504c0c83837
muriloschiler <muriloschiler.ls@gmail.com>
2022-02-18 10:24:34 -0300

    Start Project

---
+++ DrijskaConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DrijskaConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
